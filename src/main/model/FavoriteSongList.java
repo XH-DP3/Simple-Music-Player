4620b73b0f6be115111b7311195eb1087c7eb66a
Xinghao Huang <xhuang69@students.cs.ubc.ca>
2025-02-22 16:00:46 -0800

    Update FavoriteSongList class

--- src/main/model/FavoriteSongList.java
+++ src/main/model/FavoriteSongList.java
@@ -1,17 +1,11 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 // Represents a list contains songs that are marked as favorite
 public class FavoriteSongList extends SongList {
-
-    private List<Song> songs;
 
     // Construct an empty list of my favorite song
     public FavoriteSongList() {
         super();
-        songs = new ArrayList<>();
     }
 
     // REQUIRES: !favoriteSongs.contains(mySong) && mySong.isFavorite()
@@ -19,8 +13,8 @@ public class FavoriteSongList extends SongList {
     // EFFECTS: add mySong to the end of the favorite list and return true;
     @Override
     public boolean addSong(Song mySong) {
-        if (mySong.isFavorite() && !songs.contains(mySong)) {
-            songs.add(mySong);
+        if (mySong.isFavorite() && !super.getSongs().contains(mySong)) {
+            super.addSong(mySong);
             return true;
         }
         return false;
@@ -31,24 +25,12 @@ public class FavoriteSongList extends SongList {
     // return false.
     @Override
     public boolean deleteSong(String title) {
-        for (int i = 0; i < songs.size(); i++) {
-            if (songs.get(i).getTitle().equals(title)) {
-                songs.remove(i);
+        for (int i = 0; i < super.getSongs().size(); i++) {
+            if (super.getSongs().get(i).getTitle().equals(title)) {
+                super.deleteSong(i);
                 return true;
             }
         }
         return false;
-    }
-
-    // EFFECTS: return the list of my favorite songs
-    @Override
-    public List<Song> getSongs() {
-        return songs;
-    }
-
-    // EFFECTS: return the size of my favorite list.
-    @Override
-    public int getSize() {
-        return songs.size();
     }
 }
