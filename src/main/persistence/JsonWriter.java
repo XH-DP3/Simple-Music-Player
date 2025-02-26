34582d88e6e2a3002a3106e749413873923146b9
Xinghao Huang <xhuang69@students.cs.ubc.ca>
2025-02-25 16:57:37 -0800

    Added tests for JsonReader class

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -1,24 +1,28 @@
 package persistence;
 
+import java.io.FileNotFoundException;
+
+import model.SongList;
+
 // Represent a writer that writes JSON representation of songlist to file
 // for now, the methods are referenced from the below link, with several adjustments according to my own project
 // https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo/blob/master/src/main/persistence/JsonWriter.java
 public class JsonWriter {
 
     // EFFECTS: construct a writer to write to destination file
-    public JsonWriter() {
+    public JsonWriter(String destination) {
         // stub
     }
 
     // MODIFIES: this
     // EFFECTS: opens writer; throws FileNotFoundException if destination file cannot
-    public void open() {
+    public void open() throws FileNotFoundException{
         // stub
     }
 
     // MODIFES: this
     // EFFECTS: writes JSON representation of songlist to file
-    public void write() {
+    public void write(SongList mySongList) {
         // stub
     }
 
---
+++ src/test/model/TestJsonReader.java
@@ -0,0 +1,66 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.junit.Test;
+
+import persistence.JsonWriter;
+
+public class TestJsonReader {
+    private JsonWriter writer;
+
+    @Test
+    public void testOpenWithANotFoundFile() {
+        try {
+            writer = new JsonWriter("./data/3g3jcwqfj03f3].json");
+            writer.open();
+            fail("The test should not pass!");
+        } catch (FileNotFoundException e) {
+            // expected exception
+        }
+    }
+
+    @Test
+    public void testOpenWithAFoundFile() {
+        try {
+            writer = new JsonWriter("./data/myFile.json");
+            writer.open();
+        } catch (FileNotFoundException e) {
+            fail ("FileNotFoundException is not expected!");
+        }
+    }
+
+    @Test
+    public void testWriteEmptySongList() {
+        try {
+            writer = new JsonWriter("./data/myFile.json");
+            writer.open();
+            SongList mySongList = new SongList();
+            assertEquals(0, mySongList.getSize());
+            writer.write(mySongList);
+            writer.close();
+        } catch (IOException e) {
+            fail ("IOException is not expected!");
+        }
+    }
+
+    @Test
+    public void testWriteSongList() {
+        try {
+            writer = new JsonWriter("./data/myFile.json");
+            writer.open();
+            SongList mySongList = new SongList();
+            Song s1 = new Song("Payphone", "Maroon 5", "Pop", 231);
+            Song s2 = new Song("Everybody Hurts", "Avril Lavigne", "Pop", 221);
+            mySongList.addSong(s1);
+            mySongList.addSong(s2);
+            writer.write(mySongList);
+            writer.close();
+        } catch (IOException e) {
+            fail ("IOException is not expected!");
+        }
+    }
+}
