c7a5acacb1264cb08b5461406217654a5f5f7eb0
Xinghao Huang <xhuang69@students.cs.ubc.ca>
2025-02-26 18:49:42 -0800

    Added specification to JsonReader class

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -1,5 +1,54 @@
 package persistence;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.json.JSONObject;
+
+import model.FavoriteSongList;
+import model.SongList;
+
+// Represents a reader that reads song list from JSON data stored in file
+// The methods are referenced from the below link, with several adjustments according to my own project
+// https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo/blob/master/src/main/persistence/JsonWriter.java
 public class JsonReader {
     
+    // EFFECTS: constructs reader to read from source file
+    public JsonReader() {
+        // stub
+    }
+
+    // EFFECTS: reads song list from file and returns it;
+    // throws IOException if an error occurs reading data from file
+    public void read() throws FileNotFoundException{
+        // stub
+    }
+
+      // EFFECTS: reads source file as string and returns it
+    private String readFile(String source) throws IOException {
+        return "";
+        // stub
+    }
+
+    // EFFECTS: parses song list from JSON object and returns it
+    private SongList parseSongList(JSONObject jsonObject) {
+        return null;
+        // stub
+    }
+
+    // MODIFIES: mySongList
+    // EFFECTS: parses songs from JSON object and adds them to song list
+    private void addSongs(SongList mySongList, JSONObject jsonObject) {
+        // stub
+    }
+
+    // MODIFIES: myFavoriteSongList
+    // EFFECTS: parses songs from JSON object and adds them to song list
+    private void addSongs(FavoriteSongList myFavoriteSongList, JSONObject jsonObject) {
+        // stub
+    }
+
+
+
+
 }
--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -7,7 +7,7 @@ import org.json.JSONObject;
 import model.SongList;
 
 // Represent a writer that writes JSON representation of songlist to file
-// for now, the methods are referenced from the below link, with several adjustments according to my own project
+// The methods are referenced from the below link, with several adjustments according to my own project
 // https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo/blob/master/src/main/persistence/JsonWriter.java
 public class JsonWriter {
     private static final int TAB = 4;
