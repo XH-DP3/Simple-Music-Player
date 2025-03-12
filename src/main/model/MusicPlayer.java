e39587de7fe9a9e5cd3ca740f9a15fc461edcef2
Xinghao Huang <xhuang69@students.cs.ubc.ca>
2025-03-11 17:40:45 -0700

    Added specification to MusicPlayer class

---
+++ src/main/model/MusicPlayer.java
@@ -0,0 +1,27 @@
+package model;
+
+// Represents a music player that will play the melody of the music
+public class MusicPlayer {
+    
+    // MODIFIES: this
+    // EFFECTS: construct a music player object with a filepath
+    public MusicPlayer(String filepath) {
+        // stub
+    }
+
+    // EFFECTS: play the music
+    public void play() {
+        // stub
+    }
+
+    // EFFECTS: pause the music
+    public void pause() {
+        // stub
+    }
+
+    // EFFECTS: return true if the music is over
+    public boolean isOver() {
+        // stub
+        return false;
+    }
+}
